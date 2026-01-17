95bf439f56e70b40368d1041b12ceb9e37d69c7d
Eric Apostal <ehapostal04@gmail.com>
2026-01-16 23:35:28 -0500

    linux header bar

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -43,13 +43,11 @@ static void my_application_activate(GApplication* application) {
   }
 #endif
   if (use_header_bar) {
-    GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
-    gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "hyloader");
-    gtk_header_bar_set_show_close_button(header_bar, TRUE);
-    gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
+    gtk_window_set_decorated(window, TRUE);
+    gtk_window_set_titlebar(window, NULL); // disables CSD
+    gtk_window_set_title(window, "Hyloader");
   } else {
-    gtk_window_set_title(window, "hyloader");
+    gtk_window_set_title(window, "Hyloader");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
