2ffbc1e9a981149c5d7b6206b54834eb2085bd84
Eric Apostal <ehapostal04@gmail.com>
2026-01-16 21:02:11 -0500

    hard coded works

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hyapi/hyapi.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:process_run/process_run.dart';
 
 void main() {
   runApp(const MainApp());
@@ -30,6 +31,23 @@ class MainApp extends StatelessWidget {
               print("launcher data = ${launcherData}");
               final session = await client.sessions.create();
               print("got session = ${session}");
+
+              final profile = client.launcherData.profiles.first;
+              final name = profile.username;
+              final uuid = profile.id;
+              final identityToken = session.identityToken;
+              final sessionToken = session.sessionToken;
+
+              final clientPath =
+                  "/home/eric/.var/app/com.hypixel.HytaleLauncher/data/Hytale/install/release/package/game/latest/Client/HytaleClient";
+
+              final command =
+                  "$clientPath --name $name --uuid $uuid --identity-token $identityToken --session-token $sessionToken";
+              print("command");
+              print(command);
+              final shell = Shell();
+
+              shell.run(command);
             },
             child: Text("Yo"),
           ),
